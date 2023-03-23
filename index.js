563a94aca66fdb9c75b8f9b50c12e3f57c7d3b52
Tugsoo19 <tugswork@gmail.com>
2023-03-23 12:25:38 +0800

    cloudinary postman

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
 require("dotenv").config();
+const cloudinary = require("./config/cloudinary")
+const uploader = require("./config/config")
+
+
 
 const PORT = process.env.PORT;
 const MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING;
@@ -17,6 +20,13 @@ app.get("/", (request, response) => {
     });
 });
 
+app.post("/upload", uploader.single("file"), async (req, res) => {
+    const upload = await cloudinary.v2.uploader.upload(req.file.path);
+    return res.json({
+        success: true,
+        file: upload.secure_url,
+    })
+})
 
 
 
